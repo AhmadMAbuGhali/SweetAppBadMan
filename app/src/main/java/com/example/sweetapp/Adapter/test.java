3968ebd9d54663f9da278a33231d3e00c92f7536
BadMan1999 <47474140+BadMan1999@users.noreply.github.com>
2021-04-19 03:55:26 +0300

    Test

--- app/src/main/java/com/example/sweetapp/Adapter/test.java
+++ app/src/main/java/com/example/sweetapp/Adapter/test.java
@@ -2,9 +2,10 @@ package com.example.sweetapp.Adapter;
 
 public class test {
     String a;
+    int aa=10;
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello"+"hi");
     }
 
 }
